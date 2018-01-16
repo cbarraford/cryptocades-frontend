ad30ea6058164bfca91b566881f25b955a1a0af2
Chad Barraford <cbarraford@gmail.com>
2018-01-16 07:03:47 +0000

    fix staging base url

--- src/Client.js
+++ src/Client.js
@@ -5,7 +5,7 @@ class Client {
     let baseURL = '';
     if (process.env.NODE_ENV === 'staging') {
       // TODO: remove staging address from client side code
-      baseURL = 'lotto-staging.herokuapp.com';
+      baseURL = 'https://lotto-staging.herokuapp.com';
     } else {
       baseURL = 'http://localhost:8080';
     }
