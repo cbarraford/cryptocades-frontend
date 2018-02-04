58c9deafda69f3c3b75e2a5662a7134a98245da5
Chad Barraford <cbarraford@gmail.com>
2018-02-05 01:00:58 +0200

    remove debug print lines

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -90,10 +90,8 @@ module.exports = function(proxy, allowedHost) {
     before(app) {
       app.all('*', function (req, res, next) {
 
+        // ensure we only allow https when we support ssl
         if (process.env.HTTPSONLY && req.headers['x-forwarded-proto'] === "http") {
-          console.log("Secure", req.secure)
-          console.log("Conn", req.connection.encrypted)
-          console.log("Protocol", req.headers['x-forwarded-proto'])
           res.redirect('https://' + req.headers.host + req.url);
         } else if (process.env.AUTH_USER && process.env.AUTH_PASSWORD) {
           var credentials = auth(req)
