7819a3740bfd5b9b6a3170b6ccd200427f0ab8e7
Chad Barraford <cbarraford@gmail.com>
2018-03-08 18:48:45 -0300

    redesign jackpot page

--- src/components/Jackpots.js
+++ src/components/Jackpots.js
@@ -3,6 +3,37 @@ import { Link } from 'react-router-dom'
 import { inject, observer } from 'mobx-react';
 import Countdown from 'react-countdown-now';
 import dateFormat from 'dateformat'
+import odds from 'odds';
+
+// Renderer callback with condition
+export function renderer({ hours, minutes, seconds, completed }) {
+  if (completed) {
+    // Render a completed state
+    return (
+      <span>Jackpot ended!</span>
+    )
+  } else {
+    // Render a countdown
+    let parts = []
+    const days = Math.floor(hours / 24)
+    if (days > 0) {
+      parts.push(days + " days")
+    }
+    hours = hours % 24
+    if (hours > 0 && parts.length < 2) {
+      parts.push(hours + " hours")
+    }
+    if (minutes > 0 && parts.length < 2) {
+      parts.push(minutes + " minutes")
+    }
+    if (seconds > 0 && parts.length < 2) {
+      parts.push(seconds + " seconds")
+    }
+    return (
+      <span>{parts.join(' and ')}</span>
+    )
+  }
+}
 
 @inject('store')
 @inject('client')
@@ -11,47 +42,135 @@ class Jackpots extends Component {
 
   constructor(props) {
     super(props)
-    
+
     this.state = {
       jackpots: [],
+      total_entries: 0,
+      my_entries: 0,
     }
 
     this.props.client.listJackpots()
       .then((response) => {
-        this.setState({jackpots: response.data })
+        this.setState({jackpots: response.data }, () => {
+          this.updateOdds(response.data[0].id)
+        })
       })
       .catch((error) => {
         this.props.client.handleError(error, "Failed to get list of jackpots")
       })
 
+    this.updateOdds = this.updateOdds.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.id]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.jackpots.length > 0) {
+      const id = this.state.jackpots[0].id
+      const amount = parseInt(this.state.pending_entries, 10);
+      this.props.client.jackpotEnter(id, amount)
+        .then((response) => {
+          this.setState({ pending_entries: 0 }, () => {
+            this.refs.pending_entries.value = 0
+          })
+          this.updateOdds(id)
+          this.props.client.balance()
+            .then((response) => {
+              this.props.store.balance = response.data.balance
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        })
+        .catch((error) => {
+          this.props.client.handleError(error, "Failed to enter into jackpot")
+        })
+    }
+  }
+
+  updateOdds(id) {
+    this.props.client.jackpotOdds(id)
+      .then((response) => {
+        this.setState({
+          total_entries: response.data.total,
+          my_entries: response.data.entries,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   render() {
-    const { jackpots } = this.state;
+    const { jackpots, total_entries, my_entries } = this.state;
     const { btcPrice } = this.props.store;
+    const o = odds(my_entries, total_entries)
+    const attrPanel = {minHeight: "140px", backgroundColor: "#57e5c4", color: "#266586"}
 
     var currentJackpots = jackpots.map((jackpot) => {
       if (Date.parse(jackpot.end_time) < Date.now()) {
         return ""
       }
       return (
-        <div key={jackpot['id']} className="col-lg-3 col-sm-6">
-          <div className="thumbnail">
-            <div className="thumb">
-              <Link to={"/games/" + jackpot['id']}>
-                <img src="assets/images/placeholder.jpg" alt="" />
-                <div className="caption-overflow">
+        <div key={jackpot['id']}>
+          <div className="row">
+
+            <div className="col-lg-4">
+              <div className="panel" style={attrPanel}>
+                <div className="panel-body text-center">
+                  <span style={{fontSize:"30px", fontWeight: 600}}>Prize</span>
+                  <h3 className="no-margin" style={{fontSize: "20px"}}><i class="fab fa-bitcoin"></i> {jackpot.jackpot}</h3>
+                  <div className="text-size-small">
+                    (valued at ~${(jackpot['jackpot'] * btcPrice.usd).toFixed(2)} USD)
+                  </div>
                 </div>
-              </Link>
+              </div>
             </div>
 
-            <div className="caption">
-              <h6 className="no-margin-top text-semibold">
-                <Link to={"/games/" + jackpot['id']} className="text-default">
-                  Amount: ${(jackpot['jackpot'] * btcPrice.usd).toFixed(2)}
-                </Link>
-              </h6>
-              Countdown: <Countdown date={Date.parse(jackpot['end_time'])} />
+            <div className="col-lg-4">
+              <div className="panel" style={attrPanel}>
+                <div className="panel-body text-center">
+                  <span style={{fontSize:"30px", fontWeight: 600}}>Your Odds</span>
+                  <h3 className="no-margin">{my_entries > 0 ? o.format('fractional'): "You haven't entered this jackpot"}</h3>
+                  <div className="text-size-small">
+                    (total entries: {total_entries})
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className="panel" style={attrPanel}>
+                <div className="panel-body text-center">
+                  <span style={{fontSize:"30px", fontWeight: 600}}>Countdown</span>
+                  <h6 className="">
+                    <Countdown date={Date.parse(jackpot['end_time'])} renderer={renderer}/>
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="panel panel-flat border-top-xlg border-top-warning col-lg-6 col-lg-offset-3" style={{borderTopColor: "#57e5c4"}}>
+              <div className="panel-heading">
+                <h6 className="panel-title"><span className="text-semibold">Enter amount of plays to enter</span></h6>
+              </div>
+
+              <div className="panel-body">
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group has-feedback">
+                    <input type="number" autoComplete="off" className="form-control" placeholder="Amount" onChange={this.handleChange} ref="pending_entries" id="pending_entries" />
+                  </div>
+
+                  <button type="submit" className="btn btn-block" style={{backgroundColor: "#266586", color: "#fdfbf0"}}>Enter Jackpot</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -63,7 +182,7 @@ class Jackpots extends Component {
         return ""
       }
       return (
-        <div key={jackpot['id']} className="col-lg-3 col-sm-6">
+        <div key={jackpot['id']}>
           <div className="thumbnail">
             <div className="thumb">
               <Link to={"/games/" + jackpot['id']}>
@@ -90,18 +209,12 @@ class Jackpots extends Component {
       <div className="page-container" style={{minHeight: "68px"}}>
         <div className="page-content">
           <div className="content-wrapper">
-            <h6 className="content-group text-semibold">
-              Jackpots
-            </h6>
+            <h1 className="content-group text-semibold content-header text-center">
+              Current Jackpot
+            </h1>
 
             <div className="row">
-              <h6>Current Jackpots</h6>
-              {currentJackpots}
-            </div>
-            <hr />
-            <div className="row">
-              <h6>Past Jackpots</h6>
-              {completedJackpots}
+                {currentJackpots}
             </div>
           </div>
         </div>
--- src/components/SecondNav.js
+++ src/components/SecondNav.js
@@ -2,38 +2,7 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom'
 import Countdown from 'react-countdown-now';
-
-// Random component
-const Completionist = () => <span>Jackpot ended!</span>;
-
-// Renderer callback with condition
-const renderer = ({ hours, minutes, seconds, completed }) => {
-  if (completed) {
-    // Render a completed state
-    return <Completionist />;
-  } else {
-    // Render a countdown
-    let parts = []
-    const days = Math.floor(hours / 24)
-    if (days > 0) {
-      parts.push(days + " days")
-    }
-    hours = hours % 24
-    if (hours > 0) {
-      parts.push(hours + " hours")
-    }
-    if (minutes > 0) {
-      parts.push(minutes + " minutes")
-    }
-    if (seconds > 0) {
-      //parts.push(seconds + " seconds")
-    }
-    if (parts.length > 1) {
-      parts.splice(parts.length-1, 0, "and");
-    }
-    return <span>{parts.join(' ')}</span>;
-  }
-};
+import { renderer } from './Jackpots'
 
 @inject('store')
 @inject('client')
@@ -110,7 +79,7 @@ class SecondNav extends Component {
             <br />
             <span>
               <span style={{fontSize: "30px", fontWeight:600}}>
-                {jackpot} BTC ≈ ${(jackpot * btcPrice.usd).toFixed(2)}
+                <i class="fab fa-bitcoin"></i> {jackpot} ≈ ${(jackpot * btcPrice.usd).toFixed(2)}
               </span>
             </span>
           </div>
