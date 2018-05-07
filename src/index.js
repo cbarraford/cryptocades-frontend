5c153f39bf72fcd7c86e027fc231678c941cf8d8
Chad Barraford <cbarraford@gmail.com>
2018-05-07 17:13:34 -0500

    redirect to maint page

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ ReactDOM.render((
   <Provider {...stores}>
   <Router history={history}>
     <div>
+      <Route path='/' component={() => window.location = 'https://cryptocades.com/maintenance'}/>
       <MatchWithDefaultLayout exact path="/" component={Login}/>
       <MatchWithDefaultLayout exact path="/signup" component={Signup}/>
       <MatchWithDefaultLayout exact path="/password/forget" component={ForgetPassword}/>
